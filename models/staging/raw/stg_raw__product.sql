cda477e83db18dd8fc2c9680125f655c3dc47c2b
Ghita Bensalah <ghitabensa@gmail.com>
2023-10-31 14:06:33 +0000

    correct

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -10,7 +10,7 @@ renamed as (
 
     select
         products_id,
-        purchase_price
+        purchse_price AS puchase_price
 
     from source
 
--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -18,7 +18,6 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        shipping_fee_1,
         logcost,
         ship_cost
 
